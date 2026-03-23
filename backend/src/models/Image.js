c43daf8affc7a47314bff0a91953ed11bc494550
shilat <shilatr2007@gmail.com>
2026-03-23 11:54:17 +0200

    image schema done

--- backend/src/models/Image.js
+++ backend/src/models/Image.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // =============================================================
 // TODO: Define the Image Schema
@@ -33,18 +33,44 @@ const mongoose = require('mongoose');
 const imageSchema = new mongoose.Schema(
   {
     // TODO: Define userId field here
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User ID is required"],
+    },
 
     // TODO: Define filename field here
 
+    filename: {
+      type: String,
+      required: [true, "Filename is required"],
+    },
+
     // TODO: Define s3Key field here
+
+    s3Key: {
+      type: String,
+      required: [true, "S3 key is required"],
+    },
 
     // TODO: Define s3Url field here
 
+    s3Url: {
+      type: String,
+      required: [true, "S3 URL is required"],
+    },
+
     // TODO: Define status field here
+
+    status: {
+      type: String,
+      enum: ["pending", "processed"],
+      default: "pending",
+    },
   },
   {
     timestamps: true,
-  }
+  },
 );
 
-module.exports = mongoose.model('Image', imageSchema);
+module.exports = mongoose.model("Image", imageSchema);
