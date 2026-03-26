d6f6695f9dbe1f1e84b97a44fb54a58d18af4934
Shilo Even Tzur <shilo.even@hotmail.com>
2026-03-26 17:22:25 +0200

    TanStack Query setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      retry: 2,
+      gcTime: 1000 * 60 * 10, // 10 minutes
+      refetchOnWindowFocus: true,
+    },
+  },
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
